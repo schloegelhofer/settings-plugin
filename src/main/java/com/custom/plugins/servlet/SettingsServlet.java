a8be0898fa58d3f4f81c2403190f268ba87223fb
schloegelhofer <dan.schloegelhofer@gmail.com>
2024-12-18 14:15:26 +0100

    added velociities and servlets

---
+++ src/main/java/com/custom/plugins/servlet/SettingsServlet.java
@@ -0,0 +1,37 @@
+package com.custom.plugins.servlet;
+
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import com.atlassian.templaterenderer.TemplateRenderer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SettingsServlet extends HttpServlet {
+    private static final Logger log = LoggerFactory.getLogger(SettingsServlet.class);
+
+    @ComponentImport
+    private final TemplateRenderer templateRenderer;
+
+    public SettingsServlet(TemplateRenderer templateRenderer) {
+        this.templateRenderer = templateRenderer;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+
+        resp.setContentType("text/html;charset=utf-8");
+
+        // Der Template-Pfad relativ zum Verzeichnis src/main/resources/templates
+        String templatePath = "templates/settings.vm";
+
+        // Template rendern
+        templateRenderer.render(templatePath, resp.getWriter());
+    }
+
+}
